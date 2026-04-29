ca3c16a2e8d1f8c92b4df9c087761c4c4c42d253
Farenheith <jacopo.barnabe@gmail.com>
2026-04-29 15:36:08 +0000

    Builded loops project

---
+++ loops/target/debug/deps/loops-63b8616c0139b007.d
@@ -0,0 +1,5 @@
+/workspaces/Rust_Book_Projects/loops/target/debug/deps/loops-63b8616c0139b007.d: src/main.rs
+
+/workspaces/Rust_Book_Projects/loops/target/debug/deps/loops-63b8616c0139b007: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/workspaces/Rust_Book_Projects/loops/target/debug/loops: /workspaces/Rust_Book_Projects/loops/src/main.rs
