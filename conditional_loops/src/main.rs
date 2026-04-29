8b61229056c411aab29b64db7b58cfc640b0f128
Farenheith <jacopo.barnabe@gmail.com>
2026-04-29 15:44:39 +0000

    Optimized LIFTOFF with for loop

--- conditional_loops/src/main.rs
+++ conditional_loops/src/main.rs
@@ -1,12 +1,7 @@
 fn main() {
-    let mut number = 3;
-
-    while number != 0 {
+    for number in (1..4).rev() {
         println!("{number}!");
-
-        number -= 1;
     }
-
     println!("LIFTOFF!!!");
 
     looping_array();
