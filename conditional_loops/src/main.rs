005aa1583c07684857c34c7e3876b5780c8c50f6
Farenheith <jacopo.barnabe@gmail.com>
2026-04-29 15:41:23 +0000

    Added a loop through array elements

--- conditional_loops/src/main.rs
+++ conditional_loops/src/main.rs
@@ -8,4 +8,17 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+    looping_array();
+}
+
+fn looping_array() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
 }
